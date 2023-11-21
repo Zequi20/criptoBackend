57198710c809cf57a4608106f6b5b711485ec05c
Zequi20 <ezequiel234242@gmail.com>
2023-11-21 10:42:20 -0300

    mysql connection

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify
+import mysql.connector as mariadb
 import requests
 import datetime
 
@@ -8,7 +9,6 @@ app = Flask(__name__)
 @app.route('/top_monedas')
 def mostrar_top_monedas():
     top_monedas = obtener_top_monedas()
-
     if top_monedas is not None:
         return jsonify(top_monedas)
     else:
@@ -16,12 +16,10 @@ def mostrar_top_monedas():
 
 def obtener_top_monedas():
     url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=5&tsym=USD'
-    
     headers = {
         'Apikey': API_KEY
     }
     respuesta = requests.get(url, headers=headers)
-    
     if respuesta.status_code == 200:
         respuesta_json = respuesta.json()
         respuesta_filtrada = map(filtrar_datos, respuesta_json["Data"])
@@ -30,7 +28,6 @@ def obtener_top_monedas():
         return None
 
 def filtrar_datos(elem):
-    
     return {
         "name": elem["CoinInfo"]["FullName"],
         "symbol": elem["CoinInfo"]["Internal"],
@@ -43,39 +40,49 @@ def filtrar_datos(elem):
 @app.route('/historial_precio/<symbol>')
 def obtener_historial_precio(symbol):
     historial_precio = obtener_historial_precio_moneda(symbol)
-
     if historial_precio is not None:
         return jsonify(historial_precio)
     else:
         return 'Error al consultar el historial de precio'
-
+    
 def obtener_historial_precio_moneda(symbol):
     url = 'https://min-api.cryptocompare.com/data/v2/histoday'
-    
     # Calcular la fecha de inicio y fin para los últimos 30 días
     fecha_fin = datetime.datetime.now()
-    
-    
     parametros = {
-        'fsym': symbol,                 # Símbolo de la criptomoneda
-        'tsym': 'USD',                  # Moneda de cotización (USD)
-        'limit': 30,                    # Número de puntos de datos históricos (últimos 30 días)
-        'toTs': int(fecha_fin.timestamp()),     # Fecha de finalización en formato Unix timestamp
-        'e': 'CCCAGG'                   # Intercambio de referencia (CryptoCompare Aggregate)
+        'fsym': symbol,                 
+        'tsym': 'USD',                 
+        'limit': 30,                   
+        'toTs': int(fecha_fin.timestamp()),     
+        'e': 'CCCAGG'                   
     }
-    
     headers = {
         'Apikey': API_KEY
     }
-    
     respuesta = requests.get(url, params=parametros, headers=headers)
-    
     if respuesta.status_code == 200:
         data = respuesta.json()
         return data['Data']['Data']  # Devolver los datos de precio histórico
     else:
         return None
 
+@app.route('/perfiles', methods=['GET'])
+def get_perfiles():
+    try:
+        conn = mariadb.connect(user='root', password='root', database='crypto')
+        cur = conn.cursor()
+        sql = 'SELECT * FROM profile'
+        cur.execute(sql)
+        columns = [column[0] for column in cur.description]
+        rows = [dict(zip(columns, row)) for row in cur.fetchall()]
+        return jsonify(rows)
+    except mariadb.Error as e:
+        print(f"Error: {e}")
+    finally:
+        cur.close()
+        conn.close()
+
 
 if __name__ == '__main__':
     app.run(debug=True)
+
